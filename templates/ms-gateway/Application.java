a4d95bb4ba0c8892e94058dd5a1399714c6ed07b
Sergey Vinichenko <winechess@gmail.com>
2017-04-09 22:42:30 +0300

    Add gateway

---
+++ templates/ms-gateway/Application.java
@@ -0,0 +1,17 @@
+package ${ group };
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ${appName} {
+    public static void main(String[] args) {
+        SpringApplication.run(${appName}.class, args);
+    }
+}
--- templates/ms-root/lazybones.groovy
+++ templates/ms-root/lazybones.groovy
@@ -1,11 +1,18 @@
 def props = [:]
 
-props.group = ask('Group [com.example]: ', 'com.example')
+props.group = ask('Group [com.example]: ', 'com.example').replace('.' as char, '/' as char).replace('-' as char, '_' as char)
 props.version = ask('Выберите версию [0.0.1]: ', '0.0.1')
 props.projectName = props.group.split('\\.').last()
 props.dockerHost = "192.168.99.100"
 props.dns = "172.17.42.1"
 
+String rebuildFileName = 'rebuild-all'
+
 processTemplates "docker-compose.yml", props
 processTemplates "settings.gradle", props
 processTemplates "build.gradle", props
+processTemplates rebuildFileName, props
+
+
+Path templateRebuildScriptPath = templateDir.toPath().resolve
+setPosixFilePermissions templateRebuildScriptPath, PosixFilePermissions.fromString("rwxr-xr-x")
