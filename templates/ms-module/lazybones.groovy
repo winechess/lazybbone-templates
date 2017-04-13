446b51da35d72140af60a80202493d4eb1f32d4e
Sergey Vinichenko <winechess@gmail.com>
2017-04-13 20:46:00 +0300

    Add ROOT Log Level

--- templates/ms-module/lazybones.groovy
+++ templates/ms-module/lazybones.groovy
@@ -186,15 +186,15 @@ void addProjectToDockerCompose(projectPath, props) {
     dockerComposeFile.withWriter { writer ->
         currentConfigDump.eachLine { line ->
             if (line.contains('end includes')) {
-                writer << "\t${projectDir.name}:"
+                writer << "${projectDir.name}:"
                 writer << '\n'
-                writer << "\t\timage: ${projectDir.name}"
+                writer << "  image: ${projectDir.name}"
                 writer << '\n'
-                writer << "\t\trestart: always"
+                writer << "  restart: always"
                 writer << '\n'
-                writer << "\t\tports:"
+                writer << "  ports:"
                 writer << '\n'
-                writer << "\t\t\t- ${props.serverPort}:${props.serverPort}"
+                writer << "    - ${props.serverPort}:${props.serverPort}"
                 writer << '\n'
             }
 
